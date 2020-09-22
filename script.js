9511f1ce512361a5a6a377837c32a3cf62a02c4f
Sacha <urswiit@gmail.comgit statusgit config --global user.email urswiit@gmail.com>
2020-09-22 15:33:20 +0200

    ajout js & modif css

---
+++ script.js
@@ -0,0 +1,11 @@
+var age = 28;
+
+if(age > 18){
+  console.log('L\'utilisateur est majeur');
+}
+else if(age == 18){
+  console.log('Egale à 18');
+}
+else {
+  console.log('L\'utilisateur est mineur');
+}
